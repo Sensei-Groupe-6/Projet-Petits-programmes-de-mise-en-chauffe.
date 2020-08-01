6fedca404eb9c6cf231dfbd2df4a49ed38954dd5
Sensei-Groupe-6 <69072850+Sensei-Groupe-6@users.noreply.github.com>
2020-08-01 12:56:14 +0300

    Update 00_multiple_spec.rb

--- spec/00_multiple_spec.rb
+++ spec/00_multiple_spec.rb
@@ -1,42 +1,19 @@
 require_relative '../lib/00_multiple'
 
-describe "#is_multiple_of_3_or_5?" do
+describe "the sum_of_3_and_5_multiples method" do  
 
-  it "check if 3 or 5 are multiple of 3 or 5" do
-    s = is_multiple_of_3_or_5?(3||5)
-    expect(s).to eq(true)
-  end
+	it "should return the sum of multiples of 3 and 5 lower than the input" do
+		expect(sum_of_3_and_5_multiples(10)).to eq(23)    
 
-  it "check if 51 or 45 are multiple of 3 or 5" do
-    s = is_multiple_of_3_or_5?(51||45)
-    expect(s).to eq(true)
-  end
+		expect(sum_of_3_and_5_multiples(11)).to eq(33)    
 
-  it "check if 2, 7 or 64 are not multiple of 3 or 5" do
-    s = is_multiple_of_3_or_5?(2||7||64)
-    expect(s).to eq(false)
-  end
-end
+		expect(sum_of_3_and_5_multiples(0)).to eq(0)    
 
-describe "#sum_of_3_or_5_multiples" do
+		expect(sum_of_3_and_5_multiples(3)).to eq(0)  
+	end
 
-  it "check if the sum for 10 give 23" do
-    s = sum_of_3_or_5_multiples(10)
-    expect(s).to eq(23)
-  end
-
-  it "check if the sum for 11 give 33" do
-    s = sum_of_3_or_5_multiples(11)
-    expect(s).to eq(33)
-  end
-
-  it "check if the sum for 0 or 3 give 0" do
-    s = sum_of_3_or_5_multiples(0||3)
-    expect(s).to eq(0)
-  end
-
-  it "check if the sum for -1, 1.23 or 'chiffre' give 0" do
-    s = sum_of_3_or_5_multiples(-1||1.23||"chiffre")
-    expect(s).to eq("Yo ! Je ne prends que les entiers naturels. TG")
-  end
+	it "should not calculate if the input is not a naturalnumber" do
+		expect(sum_of_3_and_5_multiples(-1||1.23||"chiffre")).to eq("yo je ne prends que les entiers naturels TG")
+	end
+	
 end
