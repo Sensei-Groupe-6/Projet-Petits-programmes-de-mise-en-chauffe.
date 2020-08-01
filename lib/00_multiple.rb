389d8e1126d22de13eb85fba38af2088d35ad834
Sensei-Groupe-6 <69072850+Sensei-Groupe-6@users.noreply.github.com>
2020-08-01 18:35:51 +0300

    Update 00_multiple.rb
    Nampiako commentaire!! dia nocoloreko Rouge le texte erreur!! 
    By Fano

--- lib/00_multiple.rb
+++ lib/00_multiple.rb
@@ -1,22 +1,27 @@
 #!/usr/bin/env ruby
 
-#  Fonction de calculer la somme des nombre naturel 
-#  multiple à 3 et 5 au dessous de nombre indiquer
+#  Description project : 
+#  Fonction permettant de calculer la somme des nombres entiers
+#  multiple de 3 et de 5 inférieur ou égal au nombre indiquer
+
+# Define function
 
 def sum_of_3_and_5_multiples(n)
-	x = 0
-	y = []
-	if n.is_a?(Integer) && n >= 0
-		until x == n
-			if x % 3 == 0 || x % 5 == 0
-				y << x
-			end
-			x += 1
-		end
-		return y.reduce(0, :+)
-	else
-		return "ERREUR!!!!yo je ne prends que les entiers naturels TG"
-	end
-end
+	x = 0  	# counter
+	y = []	# storing the sum of numbers
 
-puts sum_of_3_and_5_multiples(10)
+	if n.is_a?(Integer) && n >= 0			# test : the number input is integer and positive
+		
+		until x == n
+			if x % 3 == 0 || x % 5 == 0		# test : less or equal input number is multiple of 3 and 5
+				y << x						# x addition y		
+			end
+			x += 1				
+		end
+
+		return y.reduce(0, :+)				# Result of sum
+	else
+		# when the input is not a number or integer!  
+		return "\033[31m" + "ERREUR!!!! yo je ne prends que les entiers naturels TG" #colored red
+	end
+end	
