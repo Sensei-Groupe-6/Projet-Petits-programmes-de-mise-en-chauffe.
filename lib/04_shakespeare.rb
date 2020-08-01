6073ec7e2eeaab4de404347074702f60cc1ffb74
Sensei-Groupe-6 <69072850+Sensei-Groupe-6@users.noreply.github.com>
2020-08-01 12:22:48 +0300

    Update 04_shakespeare.rb

--- lib/04_shakespeare.rb
+++ lib/04_shakespeare.rb
@@ -1,14 +1,19 @@
-dictionnary1 = ["below", "down", "go", "going", "horn", "how", "howdy", "it", "i", "low", "own", "part", "partner", "sit"]
-dictionnary2 = ["the", "of", "and", "to", "a", "in", "for", "is", "on", "that", "by", "this", "with", "i", "you", "it", "not", "or", "be", "are"]
-book = File.read("data/shakespeare.txt")
-banned = File.read("data/banned.txt").split("\n")
+# Initialisation Variables
+  dictionnary1 = ["below", "down", "go", "going", "horn", "how", "howdy", "it", "i", "low", "own", "part", "partner", "sit"]
+  dictionnary2 = ["the", "of", "and", "to", "a", "in", "for", "is", "on", "that", "by", "this", "with", "i", "you", "it", "not", "or", "be", "are"]
+  book = File.read("data/shakespeare.txt")  #read book shakespeare
+  banned = File.read("data/banned.txt").split("\n")  # dictionnary words banned
 
-def word_counter(word,dictionnary)
-hash = {}
-dictionnary.map{|i|
-if word.downcase.include?(i) == true
-hash[i] = word.downcase.scan(i).count
-end
-}
-return hash
-end 
+# Definition function
+	def word_counter(word, dictionnary)
+		hash = {nil} # Resultat initialized nil 
+		#check the table dictionnary
+		dictionnary.map{|i|
+			# test if word include in the dictionnary
+			if word.downcase.include?(i) == true
+				hash[i] = word.downcase.scan(i).count 
+			end
+		}
+	return hash
+	
+	end 
