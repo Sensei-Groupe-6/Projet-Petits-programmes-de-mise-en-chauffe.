ec07826d63cad37b332450e145e9694fe54db87b
Sensei-Groupe-6 <69072850+Sensei-Groupe-6@users.noreply.github.com>
2020-08-02 00:31:37 +0300

    Update 04_shakespeare.rb
    modification de commentaire.
    By Fano

--- lib/04_shakespeare.rb
+++ lib/04_shakespeare.rb
@@ -1,19 +1,24 @@
+#!/usr/bin/env ruby
+
 # Initialisation Variables
   dictionnary1 = ["below", "down", "go", "going", "horn", "how", "howdy", "it", "i", "low", "own", "part", "partner", "sit"]
   dictionnary2 = ["the", "of", "and", "to", "a", "in", "for", "is", "on", "that", "by", "this", "with", "i", "you", "it", "not", "or", "be", "are"]
-  book = File.read("data/shakespeare.txt")  #read book shakespeare
-  banned = File.read("data/banned.txt").split("\n")  # dictionnary words banned
+  book = File.read("data/shakespeare.txt")  # lire et stocker le livre shakespeare
+  banned = File.read("data/banned.txt").split("\n")  # lire et stocker dans un tableau les mots  bannis
 
 # Definition function
-	def word_counter(word, dictionnary)
-		hash = {nil} # Resultat initialized nil 
-		#check the table dictionnary
-		dictionnary.map{|i|
-			# test if word include in the dictionnary
-			if word.downcase.include?(i) == true
-				hash[i] = word.downcase.scan(i).count 
-			end
+def word_counter(word, dictionnary)
+	hash = {} # Résultat initialisé à nil (vide)
+		
+	# Regarder chaque element du tableau de dictionaire
+	dictionnary.map{|i|
+		# tester chaque element du tableau si cà correspond au mot de l'utilisateur
+		if word.downcase.include?(i) == true
+			# Stockage des résultat dans le tableau de résultat
+			hash[i] = word.downcase.scan(i).count 
+		end
 		}
+
+	# sortie
 	return hash
-	
-	end 
+end 
